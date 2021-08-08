1fedf94a86e7ee20bfa53ac32cbb5f22468ccc75
NRO1 <nrdevac1@gmail.com>
2021-08-08 13:27:59 +0300

    responsive adjustment

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -13,8 +13,9 @@ const CartItem = (props) => {
         </div>
       </div>
       <div className={classes.actions}>
+      <button onClick={props.onAdd}>+</button>
         <button onClick={props.onRemove}>−</button>
-        <button onClick={props.onAdd}>+</button>
+      
       </div>
     </li>
   );
